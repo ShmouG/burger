7e523c605291296d4f55e69e09cb20d3d18e41a2
Mobious <sammiel.gross@gmail.com>
2018-10-13 21:18:22 -0500

    heroku update

--- config/orm.js
+++ config/orm.js
@@ -2,93 +2,42 @@
 const connection = require ('./connection.js');
 
 // Helper function for generating MySQL syntax
-function printQuestionMarks(num) {
-	var arr = [];
-
-	for (var i = 0; i < num; i++) {
-		arr.push("?");
-	}
-
-	return arr.toString();
-}
-
-// Helper function for generating My SQL syntax
-function objToSql(ob) {
-	var arr = [];
-
-	for (var key in ob) {
-		arr.push(key + "=" + ob[key]);
-	}
-
-	return arr.toString();
-}
-
-// Create the ORM object to perform SQL queries
-const orm = {
-	// Function that returns all table entries
-	selectAll: function(tableInput, cb) {
-		// Construct the query string that returns all rows from the target table
-		var queryString = "SELECT * FROM " + tableInput + ";";
-
-		// Perform the database query
-		connection.query(queryString, function(err, result) {
-			if (err) {
-				throw err;
-			}
-
-			// Return results in callback
-			cb(result);
-		});
+const orm ={
+	selectAll: function(table, callback){
+	  var queryString = 'SELECT * FROM ' + table;
+  
+	  connection.query(queryString, function(err, data){
+		if(err) throw err;
+		callback(data);
+	  });
 	},
-
-	// Function that insert a single table entry
-	insertOne: function(table, cols, vals, cb) {
-		// Construct the query string that inserts a single row into the target table
-		var queryString = "INSERT INTO " + table;
-
-		queryString += " (";
-		queryString += cols.toString();
-		queryString += ") ";
-		queryString += "VALUES (";
-		queryString += printQuestionMarks(vals.length);
-		queryString += ") ";
-
-		// console.log(queryString);
-
-		// Perform the database query
-		connection.query(queryString, vals, function(err, result) {
-			if (err) {
-				throw err;
-			}
-
-			// Return results in callback
-			cb(result);
-		});
+  
+	insertOne: function(table, column, burgerInput, callback){
+	  var queryString = 'INSERT INTO ' + table + '(' + column + ') VALUES (?)';
+  
+	  connection.query(queryString, [burgerInput], function(err, data){
+		if(err) throw err;
+		callback(data);
+	  });
 	},
-
-	// Function that updates a single table entry
-	updateOne: function(table, objColVals, condition, cb) {
-		// Construct the query string that updates a single entry in the target table
-		var queryString = "UPDATE " + table;
-
-		queryString += " SET ";
-		queryString += objToSql(objColVals);
-		queryString += " WHERE ";
-		queryString += condition;
-
-		// console.log(queryString);
-
-		// Perform the database query
-		connection.query(queryString, function(err, result) {
-			if (err) {
-				throw err;
-			}
-
-			// Return results in callback
-			cb(result);
-		});
+  
+	updateOne: function(table, col, colVal, condition, conditionVal, callback){
+	  var queryString = 'UPDATE ' + table + ' SET ' + col + '=?' + 'WHERE ' + condition + '=?';
+  
+	  connection.query(queryString, [colVal, conditionVal], function(err, data){
+		if(err) throw err;
+		callback(data);
+	  });
+	},
+  
+	deleteOne: function(table, condition, conditionVal, callback){
+	  var queryString = 'DELETE FROM ' + table + ' WHERE ' + condition + '=?';
+  
+	  connection.query(queryString, [conditionVal], function(err, data){
+		if(err) throw err;
+		callback(data);
+	  });
 	}
-};
-
-// Export the orm object for use in other modules
-module.exports = orm;
+  };
+  
+  module.exports = orm;
