973fb4fac0944d1e81c8f11b1f10a3e515656f2f
Mobious <sammiel.gross@gmail.com>
2018-10-14 11:21:31 -0500

    heroku feex

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,29 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
 
-// Make connection.
-connection.connect( (err) => {
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'password',
+		database: 'burgers_db'
+	})
+};
+
+// Make the connection to MySQL
+connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
 });
 
-// Export connection for our ORM to use.
+// Export connection for ORM use
 module.exports = connection;
