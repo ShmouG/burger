498cabe62be0c88131c26c05e1f68d8adecdcfc0
Mobious <sammiel.gross@gmail.com>
2018-10-14 12:24:07 -0500

    heroku feex

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const PORT = process.env.PORT || 8080;
+let methodOverride = require('method-override');
 
 const app = express();
 let bodyParser = require('body-parser');
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Set Handlebars as the view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use(methodOverride('_method'));
 
 // Import routes and give the server access to them
 var routes = require('./controllers/burgers_controller.js');
